7ced6aec36b2ca1b259dbdc4c3c714b7eaea25fd
Klaus Crusius <klaus.crusius@web.de>
2017-07-03 10:51:51 +0200

    added lyapunov

--- src/sylvester.jl
+++ src/sylvester.jl
@@ -3,17 +3,29 @@ include("util.jl")
 
 using Base.LinAlg
 
-import Base.LinAlg.sylvester
+import Base.LinAlg: sylvester, lyap
 
 function sylvester(A::StridedMatrix{T},B::StridedMatrix{T},C::StridedMatrix{T}) where T<:BigFloatOrComplex
+
+  _sylvester_lyapunov(A, Nullable(B), C, sylvester)
+end
+
+
+function _sylvester_lyapunov(A::StridedMatrix{T}, B::Nullable{<:StridedMatrix{T}}, C::StridedMatrix{T}, syly::Function) where T <: BigFloatOrComplex
 
   # make standard precision copy of all input matrices calculate first approximation
   P = Float64
   S = T <: Real ? P : Complex{P}
   AS = S.(A)
-  BS = S.(B)
+  if isnull(B)
+    B = A'
+    BS = nothing
+  else
+    B = get(B)
+    BS = S.(B)
+  end
   CS = S.(C)
-  XS = sylvester(AS, BS, CS)
+  XS = syly(AS, BS, CS)
   X = T.(XS)
   rtol = eps(real(T))
   ftol = cbrt(eps(P)) # 0.9 
@@ -26,16 +38,20 @@ function sylvester(A::StridedMatrix{T},B::StridedMatrix{T},C::StridedMatrix{T}) 
     pdx = ndx * ftol
     R = A * X + X * B + C
     nres = norm(R, 1)
-    RS = S.(R)
-    dX = sylvester(AS, BS, RS)
+    CS = S.(R)
+    dX = syly(AS, BS, CS)
     ndx = norm(dX, 1)
-    X .+= dX
-    # println("nres = $(Float64(nres)/Float64(mres)), ndx = $(Float64(ndx)/Float64(mdx))")
+    if nres <= pres && ndx <= pdx
+      X .+= dX
+    end
+    println("nres = $(Float64(nres)/Float64(mres)), ndx = $(Float64(ndx)/Float64(mdx))")
   end
   X
 end
-  
 
+function lyap(A::StridedMatrix{T}, C::StridedMatrix{T}) where T<:BigFloatOrComplex
 
-
+  lyapunov(A, B, C) = lyap(A, C)
+  _sylvester_lyapunov(A, Nullable{typeof(C)}(), C, lyapunov)
+end
 
